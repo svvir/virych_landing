7423b3b2f0dfd663d286fdfc382d5c35c1cb60a3
Sviatoslav Virych <sv.virych@gmail.com>
2023-07-25 23:25:36 +0300

    Fix typo

--- src/components/hero-setion.tsx
+++ src/components/hero-setion.tsx
@@ -6,7 +6,7 @@ const HeroSection = () => {
     <img className="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded" alt="hero" src="https://global-uploads.webflow.com/5f1a1d85849b0a7727d30f10/5fa3780177fc01114acbd8a0_Product-Management_22.png"/>
     <div className="text-center lg:w-2/3 w-full">
       <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Product Manager</h1>
-      <p className="mb-8 leading-relaxed">Learning how to create web app's</p>
+      <p className="mb-8 leading-relaxed">Learning how to create web apps</p>
       <div className="flex justify-center">
         <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
         <button className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">Button</button>
